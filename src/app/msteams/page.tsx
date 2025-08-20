99a5bb76df217c4174a4f73ff9f3e22f1959fa68
Shaji-Mohammed <mshajisufiyan@gmail.com>
2025-08-20 10:36:14 -0300

    fix tech bits bug

--- src/app/msteams/page.tsx
+++ src/app/msteams/page.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import CustomHeader from '../../../components/CustomHeader'
-import CustomFooter from '../../../components/CustomFooter'
+import React from "react";
+import CustomHeader from "../../../components/CustomHeader";
+import CustomFooter from "../../../components/CustomFooter";
 
 const page = () => {
   return (
     <div>
-        <CustomHeader />
-
-        <CustomFooter />
+      <CustomHeader />
+      This page will contain information and resources about using MS Teams
+      as a TA.
+      <CustomFooter />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
